52234448aed2947785ed786f0b85472b3c1feb43
Daniel C <danielc@live.cl>
2026-05-14 00:33:21 -0400

    Actualización formulario, mensaje de enviado

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ logo.onerror = () => {
   logo.style.display = "none";
   fallback.style.display = "block";
 };
+
+const form = document.getElementById("contactForm");
+const successMessage = document.getElementById("success-message");
+
+form.addEventListener("submit", function () {
+
+  // esperar a que Google procese el envío
+  setTimeout(() => {
+
+    // ocultar formulario
+    form.classList.add("d-none");
+
+    // mostrar mensaje
+    successMessage.classList.remove("d-none");
+
+    // limpiar campos
+    form.reset();
+
+  }, 800);
+
+});
